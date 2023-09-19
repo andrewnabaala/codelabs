9713c0d10de6b2bb2c67a57b24e8cb5d508e5a30
judejudo <86878327+judejudo@users.noreply.github.com>
2023-09-20 01:53:21 +0300

    Create different excel files for specific groups, 3B and 3C

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import pandas as pd
+
+excel_file = "codelabs/data/TestFiles.xlsx"
+all_sheets= pd.read_excel(excel_file, sheet_name=None)
+
+print(all_sheets)
+for sheet_name, sheet_data in all_sheets.items():
+    output_file = f'codelabs/data/{sheet_name}.xlsx'
+    sheet_data.to_excel(output_file, index=False)
